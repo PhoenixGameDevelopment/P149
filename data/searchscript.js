1f6577552c4e104c8fc7e12f3e60f6c04a5137dd
PhoenixGameDevelopment <jackstone@phoenixgamedevelopment.com>
2014-08-08 19:49:45 +0100

    Update 8
    Initial work on advanced searching feature

--- data/searchscript.js
+++ data/searchscript.js
@@ -14,6 +14,47 @@ self.port.on("search", function(olddoc,searchterm) {
   
 }
 
-  document.body.innerHTML = document.body.innerHTML.replace(new RegExp(searchterm,'g'),searchterm.fontcolor("yellow"));
+var searchstring =   strip(document.body.innerHTML);
+
+//search for obfuscated term here:
+
+var docstring = "";
+for(i = 0; i < searchstring.length;i++){
+
+	
+	docstring = docstring + searchstring.charAt(i);
+	if(docstring.length >10){
+		docstring = docstring.substring(1,docstring.length);
+	}
+	//	console.log(searchstring.charAt(i);
+	//	console.log(docstring);
+	
+	if(evalstrings(searchterm,docstring)==true){
+		console.log("Found a Match!");
+		//highlight the match:
+		  document.body.innerHTML = document.body.innerHTML.replace(new RegExp(docstring,'g'),docstring.fontcolor("yellow"));
+
+	}
+}
+
+
+ // document.body.innerHTML = document.body.innerHTML.replace(new RegExp(searchterm,'g'),searchterm.fontcolor("yellow"));
 
 });
+
+function evalstrings(searchterm,docstring){
+	console.log("EVAL: " + searchterm + " " + docstring);
+	if(docstring.indexOf(searchterm)>-1)
+	return true;
+	else 
+		return false;
+		
+	
+}
+
+function strip(html)
+{
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = html;
+   return tmp.textContent || tmp.innerText || "";
+}
